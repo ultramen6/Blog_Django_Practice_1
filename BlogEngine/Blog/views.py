a80751d8c9c2d3d6c92e5e61d2bd8979c22171cc
ultramen6 <roman.buka@mail.ru>
2022-07-07 03:29:55 +0300

    2 часть позади

--- BlogEngine/Blog/views.py
+++ BlogEngine/Blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    n = ['a','b','c','d']
+    return render(request, 'blog/index.html', context={'names': n}) 
+ 
--- BlogEngine/BlogEngine/settings.py
+++ BlogEngine/BlogEngine/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'BlogEngine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path.joinpath(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
